b01e00500fdda83ae6d865c80d8a72b6279766bb
JeonGyeongYeon <gjeon0321@gmail.com>
2021-08-11 00:32:24 +0900

    stackOverflow company & location

--- so.py
+++ so.py
@@ -10,6 +10,13 @@ def get_last_pages():
 	last_page = pages[-2].get_text(strip=True)
 	return int(last_page)
 
+def extract_job(html):
+	title = html.find("a", {"class": "s-link"}).string
+	company, location = html.find("h3", {"class": "mb4"}).find_all("span", recursive=False)
+	company = company.get_text(strip=True)
+	location = location.get_text(strip=True)
+	return {'title': title, 'company': company, 'location': location}
+
 def extract_jobs(last_page):
 	jobs = []
 	for page in range(last_page):
@@ -17,7 +24,9 @@ def extract_jobs(last_page):
 		soup = BeautifulSoup(result.text, "html.parser")
 		results = soup.find_all("div", {"class": "-job"})
 		for result in results:
-			print(result["data-jobid"])
+			job = extract_job(result)
+			jobs.append(job)
+		return jobs
 
 def get_jobs():
 	last_page = get_last_pages()
